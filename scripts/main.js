f5f427af82075404b05815fcea72aae0f539584a
Esteban Carter <carterea1999@gmail.com>
2021-05-11 22:32:08 -0400

    Currency Code GBP Item and Price

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+console.log('Problem 1');
 const pricesArray = items.map(function(item){
   return item.price;
 
@@ -7,14 +8,29 @@ const totalPrice = pricesArray.reduce(function(acc, i){
 }, 0);
 var result = totalPrice / items.length
 console.log(result);
+console.log('Next');
 
-
-
+console.log('Problem 2');
  let filterPrices = items.filter(function(item) {
    return (item.price >= 14 && item.price < 18);
  });
+
 filterPrices.forEach(displayTitle)
 
 function displayTitle (item, index){
    console.log(item.title);
 }
+console.log('Next');
+
+
+console.log('Problem 3');
+let currency = "GBP";
+let newCurrency = items.filter(function(item) {
+  return item.currency_code == currency;
+});
+newCurrency.forEach(displayTitleAndPrice);
+
+function displayTitleAndPrice (item, index){
+   console.log(item.title, item.price);
+}
+console.log('Next');
