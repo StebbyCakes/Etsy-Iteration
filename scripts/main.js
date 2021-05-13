fc8bec11cb1fd4917cfe73a31680f33c277efab4
Esteban Carter <carterea1999@gmail.com>
2021-05-12 23:49:36 -0400

    Updated with original working answers

--- scripts/main.js
+++ scripts/main.js
@@ -15,24 +15,24 @@
   console.log('Next');
 
 
-  // Teacher Solution
-  const priceOfAllItems = items
-    .map(function(item) {
-      return item.price;
-    })
-    .reduce(function(acc, i) {
-      return acc + i;
-    });
-  // same as above solution and is known as "chaining"
-  // brings all the prices together as a map in first function and then immediately throws them into the reduce function
-  // then returns that value back into priceOfAllItems
-
-  const priceOfAllItems2 = items
-    .reduce(function(acc, item) {
-      return acc + item.price // cant do acc.price because would first time through would work but second time could look like ex: 32.price + number and there is no 32.price so error
-    }, 0); // starting at 0 makes acc be 0 and the first value of the function then becomes item.price so 0 + item.prices
-  const averagePrice = (priceOfAllItems2 / items.length).toFixed(2);
-  console.log(`The average price is $${Math.floor(averagePrice * 100 / 100)}.`)
+  // // Teacher Solution
+  // const priceOfAllItems = items
+  //   .map(function(item) {
+  //     return item.price;
+  //   })
+  //   .reduce(function(acc, i) {
+  //     return acc + i;
+  //   });
+  // // same as above solution and is known as "chaining"
+  // // brings all the prices together as a map in first function and then immediately throws them into the reduce function
+  // // then returns that value back into priceOfAllItems
+  //
+  // const priceOfAllItems2 = items
+  //   .reduce(function(acc, item) {
+  //     return acc + item.price // cant do acc.price because would first time through would work but second time could look like ex: 32.price + number and there is no 32.price so error
+  //   }, 0); // starting at 0 makes acc be 0 and the first value of the function then becomes item.price so 0 + item.prices
+  // const averagePrice = (priceOfAllItems2 / items.length).toFixed(2);
+  // console.log(`The average price is $${Math.floor(averagePrice * 100 / 100)}.`)
 
 
 
@@ -50,16 +50,16 @@
   }
 
   // Teacher Solution
-
-  .filter(function(item) {
-      return item.currency_code === "USD" && item.price >= 14 && item.price <= 18;
-    })
-    .map(function(item) {
-      return item.title;
-    }) //  chaining works by not ending the function with a semicolon until the end of the function
-    .forEach(function(item) {
-      console.log(item);
-    });
+  //
+  // filter(function(item) {
+  //     return item.currency_code === "USD" && item.price >= 14 && item.price <= 18;
+  //   })
+  //   .map(function(item) {
+  //     return item.title;
+  //   }) //  chaining works by not ending the function with a semicolon until the end of the function
+  //   .forEach(function(item) {
+  //     console.log(item);
+  //   });
 
 
   console.log('Next');
@@ -79,14 +79,14 @@
   }
 
 
-  //  Teacher solution
-
-  .filter(function(item) {
-    return item.currency_code === 'GBP';
-  })
-  .forEach(function(item){
-    console.log(`${item.title} costs '\u00a3'${item.price}`);
-  });
+  // //  Teacher solution
+  //
+  // .filter(function(item) {
+  //   return item.currency_code === 'GBP';
+  // })
+  // .forEach(function(item){
+  //   console.log(`${item.title} costs '\u00a3'${item.price}`);
+  // });
 
 
 
@@ -102,14 +102,14 @@
   });
   newMaterial.forEach(displayTitle);
 
-  // Teacher solution
-
-  .filter(function(item) {
-    return item.materials.includes('wood');
-  })
-  .forEach(function(item) {
-    console.log(`${item.title} is made of wood`)
-  });
+  // // Teacher solution
+  //
+  // .filter(function(item) {
+  //   return item.materials.includes('wood');
+  // })
+  // .forEach(function(item) {
+  //   console.log(`${item.title} is made of wood`)
+  // });
 
 
   console.log('Next');
@@ -132,17 +132,17 @@
   newMaterialSize.forEach(displayMaterials);
 
 
-// Teacher Solution
-
-  .filter(function(item) {
-    return.materials.length >= 8
-  })
-  .forEach(function(item) {
-    console.log(`${item.title} has ${item.materials.length} materials`);
-    item.materials.forEach(function(material){
-      console.log(material)
-    })
-  });
+// // Teacher Solution
+//
+//   .filter(function(item) {
+//     return.materials.length >= 8
+//   })
+//   .forEach(function(item) {
+//     console.log(`${item.title} has ${item.materials.length} materials`);
+//     item.materials.forEach(function(material){
+//       console.log(material)
+//     })
+//   });
 
   console.log('Next')
 
@@ -160,14 +160,14 @@
 
   console.log('End')
 
-// Teacher Solution
-
-const itemsMadeBySeller = itemsMadeBySeller
-.filter(function(item) {
-  return item.who_made === 'i_did'
-});
-  console.log(`${itemsMadeBySeller.length} Items were made by their sellers.`)
-// this function checks if it 'IS' that string not if it has it (basically a true of false)
+// // Teacher Solution
+//
+// const itemsMadeBySeller = itemsMadeBySeller
+// .filter(function(item) {
+//   return item.who_made === 'i_did'
+// });
+//   console.log(`${itemsMadeBySeller.length} Items were made by their sellers.`)
+// // this function checks if it 'IS' that string not if it has it (basically a true of false)
 
 
 })();
